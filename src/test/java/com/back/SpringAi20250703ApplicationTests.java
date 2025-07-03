c0190948d8626168f0b34f1cd562697914b8e4a3
jhs512 <jangka512@gmail.com>
2025-07-03 09:16:14 +0900

    work

---
+++ src/test/java/com/back/SpringAi20250703ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAi20250703ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
